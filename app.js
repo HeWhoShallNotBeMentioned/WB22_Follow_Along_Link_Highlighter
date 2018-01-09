0e459692b000915874f31992bdffa1cd0c947b62
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-08 23:11:34 -0800

    created white box for links, but it is not on the element but in upper left corner.

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const triggers = document.querySelectorAll('a');
 const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
+
+function higlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', higlightLink));
